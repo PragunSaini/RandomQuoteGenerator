99b44485ee2d90134f1e32040523638a07b0d6e0
Pragun Saini <pragunsaini117@gmail.com>
2019-08-02 15:57:06 +0530

    Updates

---
+++ precache-manifest.eab564c9f99e816ab3550cad4832dbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c4b43ed415caa19924b80866a286bf",
+    "url": "/RandomQuoteGenerator/index.html"
+  },
+  {
+    "revision": "9ff6911baba4c3c64a07",
+    "url": "/RandomQuoteGenerator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6d1c2bd7da5ff386f23a",
+    "url": "/RandomQuoteGenerator/static/js/2.ba456575.chunk.js"
+  },
+  {
+    "revision": "9ff6911baba4c3c64a07",
+    "url": "/RandomQuoteGenerator/static/js/main.887be4be.chunk.js"
+  },
+  {
+    "revision": "ba30e3cf1cb91e6143be",
+    "url": "/RandomQuoteGenerator/static/js/runtime~main.8649c393.js"
+  }
+]);
